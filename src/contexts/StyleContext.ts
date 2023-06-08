a52dd7502ca51ba42f2f5f67802ccee3f70bda6a
Jack Dunich <jadunich@gmail.com>
2023-06-08 19:49:17 -0400

    Updates

--- src/contexts/StyleContext.ts
+++
@@ -1,6 +0,0 @@
-import React from "react";
-
-const StyleContext = React.createContext(null);
-export const StyleProvider = StyleContext.Provider;
-export const StyleConsumer = StyleContext.Consumer;
-export default StyleContext;
